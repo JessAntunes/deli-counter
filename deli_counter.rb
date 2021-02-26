c9a2b0a173d25da72e0e190aeab4b959c5e6893b
Jessica Antunes <jessantunes@protonmail.com>
2021-02-26 16:12:05 -0500

    commit tree 4d9b565ee515d1e9a6107e50f716168911a5f57a

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+    
+    if array.length == 0
+        puts "The line is currently empty."  
+    else 
+        phrase = "The line is currently:"
+        array.each.with_index(1) do |person, number_in_line|
+            
+            phrase<<" #{number_in_line}. #{person}"
+        end
+        puts phrase   
+    end
+
+end
+
+# This method passes all tests
+def take_a_number(array, name)
+    array.push(name)
+    puts "Welcome, #{name}. You are number #{array.length} in line."
+    return array
+end
+
+#method passes all tests
+
+def now_serving(array)
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+    else 
+        current_customer = array.shift()
+        puts "Currently serving #{current_customer}."
+    end
+    return array
+end
